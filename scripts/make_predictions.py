3c100c7b49f5f4ae46835a5cc47f0c6c1aef0a1d
Souvik Sarkar <souviksarkar2k3@gmail.com>
2024-06-29 13:14:28 +0530

    Model testing

---
+++ scripts/make_predictions.py
@@ -0,0 +1,11 @@
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+def predict_new_data(text,tokenizer):
+  model = load_model('lstm_model')
+  sequence = tokenizer.texts_to_sequences([text])
+  padded_sequence = pad_sequences(sequence,maxlen=300,padding='post',truncating='post')
+  prediction = model.predict(padded_sequence)
+  predicted_label = prediction.argmax(axis=1)[0]
+  sentiment = {0:'Negative',1:'Neutral',2:'Positive'}[predicted_label]
+  print(f'Sentiment: {sentiment}')
