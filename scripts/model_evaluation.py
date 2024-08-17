bc490e3400aeb9c56ce14eb5934b10d87d21b193
Souvik Sarkar <souviksarkar2k3@gmail.com>
2024-08-17 16:49:34 +0530

    Probabilities are converted into category

--- scripts/model_evaluation.py
+++ scripts/model_evaluation.py
@@ -4,7 +4,7 @@ from sklearn.metrics import accuracy_score
 def evaluate_model(X_test,y_test):
   model = load_model('lstm_model')
   predictions = model.predict(X_test)
-  predicted_labels = y_test.argmax(axis=1)
-  true_labels = y_test.argmax(axis=1)
+  predicted_labels = predictions.reshape(-1) >= 0.5
+  true_labels = y_test.reshape(-1)
   accuracy = accuracy_score(true_labels,predicted_labels)
   print(f'Test Accuracy: {accuracy*100:.2f}%')
