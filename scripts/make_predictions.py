0ccd59eb75604879c6dc2ffdad3a317cbb252be9
Souvik Sarkar <souviksarkar2k3@gmail.com>
2024-08-17 16:50:22 +0530

    Tokenization length changed

--- scripts/make_predictions.py
+++ scripts/make_predictions.py
@@ -4,8 +4,8 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 def predict_new_data(text,tokenizer):
   model = load_model('lstm_model')
   sequence = tokenizer.texts_to_sequences([text])
-  padded_sequence = pad_sequences(sequence,maxlen=300,padding='post',truncating='post')
+  padded_sequence = pad_sequences(sequence,maxlen=100,padding='post',truncating='post')
   prediction = model.predict(padded_sequence)
   predicted_label = prediction.argmax(axis=1)[0]
-  sentiment = {0:'Negative',1:'Neutral',2:'Positive'}[predicted_label]
-  print(f'Sentiment: {sentiment}')
+  sentiment = {0:'Negative',1:'Positive'}[predicted_label]
+  print(f'{text}: {sentiment}')
