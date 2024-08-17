1db997846fbe922cf9a9f02d22f10ea20831949e
Souvik Sarkar <souviksarkar2k3@gmail.com>
2024-08-17 16:48:22 +0530

    Validation set is taken from Training set

--- scripts/model_training.py
+++ scripts/model_training.py
@@ -1,18 +1,20 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Embedding, LSTM, Dense
+from sklearn.model_selection import train_test_split
 
 def build_model(vocab_size,input_length):
   model = Sequential([
     Embedding(input_dim=vocab_size,output_dim=64,input_length=input_length),
     LSTM(64),
-    Dense(3,activation='softmax')
+    Dense(1,activation='sigmoid')
   ])
 
-  model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+  model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
   return model
 
-def train_model(X_train,y_train,X_val,y_val):
+def train_model(X_train,y_train):
   model = build_model(vocab_size=10000,input_length=100)
+  X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.1)
   model.fit(X_train,y_train,validation_data=(X_val,y_val),epochs=10)
   model.save('lstm_model')
   return model
