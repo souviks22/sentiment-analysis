149234879259f93cec1f67e746dbf2a5cd0445f1
Souvik Sarkar <souviksarkar2k3@gmail.com>
2024-10-08 00:00:24 +0530

    Final model driver

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -1,8 +1,8 @@
-import pandas as pd
+import numpy as np
 from sklearn.metrics import classification_report
 from tensorflow.keras import Model
 
-def evaluate(X_test: pd.Series, y_test: pd.Series, model: Model):
+def evaluate(X_test: np.ndarray, y_test: np.ndarray, model: Model):
     predictions = model.predict(X_test)
     predictions = predictions.reshape(-1) >= 0.5
     y_test = y_test.reshape(-1)
